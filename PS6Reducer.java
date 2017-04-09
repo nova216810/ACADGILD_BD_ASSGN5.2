65e94b14d0140568d59d4bbfadc7f6afdd55b11b
nova216810 <ramya1286@gmail.com>
2017-04-09 20:12:24 +0530

    Reducer for Task 6

---
+++ PS6Reducer.java
@@ -0,0 +1,24 @@
+
+	
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+public  class PS6Reducer extends Reducer<Text,IntWritable,Text,IntWritable> 
+{
+	private IntWritable units_sold = new IntWritable();
+  public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
+	{
+	      int sum = 0;
+	      System.out.println("reducer Input key = " + key.toString() );
+		    for(IntWritable val : values) 
+		    {
+	        sum = sum + val.get();
+	        System.out.println("sum = " + sum );
+	      }
+	      units_sold.set(sum);
+		    context.write(key, units_sold);
+	}
+	}
